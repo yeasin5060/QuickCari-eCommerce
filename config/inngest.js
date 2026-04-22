23de4176723bfb2ca2abe1448f84afc8580688db
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-04-22 12:05:58 +0600

    create the user update function in database

--- config/inngest.js
+++ config/inngest.js
@@ -25,3 +25,25 @@ export const syncUserCreation = inngest.createFunction(
         await User.create(userData)
     }
 )
+
+// inngest function to update user data in database
+
+export const syncUserUpdation = inngest.createFunction(
+    {
+        id : 'update-user-from-clerk'
+    }, 
+    {event : 'clerk/user.updated'},
+
+    async ({event}) => {
+        const {id , first_name , last_name , email_addresses, image_url} = event.data;
+        const userData = {
+            _id : id,
+            email : email_addresses[0].email_address,
+            name : first_name + ' ' + last_name,
+            imageUrl : image_url
+
+        }
+        await connectDb()
+        await User.findByIdAndUpdate(id , userData)
+    }
+)
