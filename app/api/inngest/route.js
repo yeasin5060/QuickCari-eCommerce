b8d8f6a7ef8a3eaaeb9e60b28229f4e79d2f71f3
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-04-23 09:55:40 +0600

    route js import change

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,5 +1,18 @@
+/*import { serve } from "inngest/next";
+import { inngest,syncUserCreation,syncUserDeletion,syncUserUpdation } from "@/config/inngest";
+
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [
+    syncUserCreation,
+    syncUserUpdation,
+    syncUserDeletion,
+  ],
+});*/
+
+
 import { serve } from "inngest/next";
-import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/inngest";
+import {inngest,syncUserCreation,syncUserDeletion,syncUserUpdation } from "../../../config/inngest";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
