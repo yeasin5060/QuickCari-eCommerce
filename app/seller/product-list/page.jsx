648ae7727bc699c47213226faeb4e19b4bdc6ec3
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-04-24 10:21:17 +0600

    product data get successfully front-end

--- app/seller/product-list/page.jsx
+++ app/seller/product-list/page.jsx
@@ -1,26 +1,44 @@
 'use client'
 import React, { useEffect, useState } from "react";
-import { assets, productsDummyData } from "@/assets/assets";
+import { assets } from "@/assets/assets";
 import Image from "next/image";
 import { useAppContext } from "@/context/AppContext";
 import Footer from "@/components/seller/Footer";
 import Loading from "@/components/Loading";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const ProductList = () => {
 
-  const { router } = useAppContext()
+  const { router , getToken , user } = useAppContext();
 
-  const [products, setProducts] = useState([])
-  const [loading, setLoading] = useState(true)
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const fetchSellerProduct = async () => {
-    setProducts(productsDummyData)
-    setLoading(false)
+    try {
+      const token = getToken();
+      const {data} = await axios.get('/api/product/seller-list',{
+         headers: { Authorization: `Bearer ${token}`}
+      });
+      console.log(data);
+      
+      if(data.success){
+        setProducts(data.product);
+        setLoading(false);
+      }else{
+        toast.error(data.message)
+      }
+    } catch (error) {
+       toast.error(error.message)
+    }
   }
 
   useEffect(() => {
-    fetchSellerProduct();
-  }, [])
+    if(user){
+      fetchSellerProduct();
+    }
+  }, [user]);
 
   return (
     <div className="flex-1 min-h-screen flex flex-col justify-between">
