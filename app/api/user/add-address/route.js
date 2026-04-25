b971639d33f08abd2addc88015f64045eb8a4ebf
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-04-25 09:18:37 +0600

    add-address route create done

--- app/api/user/add-address/route.js
+++ app/api/user/add-address/route.js
@@ -1,9 +1,19 @@
+import connectDb from "@/config/db";
+import Address from "@/models/Address";
+import User from "@/models/User";
 import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 export async function POST(request) {
     try {
         const {userId} = getAuth(request);
+        const {address} = request.json();
+
+        await connectDb();
+        const newAddress = await Address.create({...address , userId});
+
+        return NextResponse.json({success : true , message : 'Address added successfully', newAddress})
     } catch (error) {
-        
+        return NextResponse.json({success : false , message :error.message})
     }
 }
