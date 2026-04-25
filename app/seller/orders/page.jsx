23365d6b3de082b7888379dd3000bf5002005db4
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-04-25 19:40:32 +0600

    get the order data seller order page

--- app/seller/orders/page.jsx
+++ app/seller/orders/page.jsx
@@ -5,22 +5,40 @@ import Image from "next/image";
 import { useAppContext } from "@/context/AppContext";
 import Footer from "@/components/seller/Footer";
 import Loading from "@/components/Loading";
+import toast from "react-hot-toast";
+import axios from "axios";
 
 const Orders = () => {
 
-    const { currency } = useAppContext();
+    const { currency , getToken , user} = useAppContext();
 
     const [orders, setOrders] = useState([]);
     const [loading, setLoading] = useState(true);
 
     const fetchSellerOrders = async () => {
-        setOrders(orderDummyData);
-        setLoading(false);
+       try {
+            const token = await getToken();
+            const {data} = await axios.get('/api/order/seller-orders', {
+                headers: { Authorization: `Bearer ${token}`}
+            });
+            console.log(data);
+            
+            if(data.success){
+                setOrders(data.orders);
+                setLoading(false)
+            }else{
+                toast.error(data.message);
+            }
+       } catch (error) {
+            toast.error(error.message);
+       }
     }
 
     useEffect(() => {
-        fetchSellerOrders();
-    }, []);
+        if(user){
+            fetchSellerOrders();
+        }
+    }, [user]);
 
     return (
         <div className="flex-1 h-screen overflow-scroll flex flex-col justify-between text-sm">
